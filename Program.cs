90c7d5091ab271bd423cd4d3e46d4584e7bddd23
Gus Catalano <guscatalano@users.noreply.github.com>
2025-02-09 19:06:08 -0800

    it somewhat works

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RDP_DVC_Watcher;
+
+ETWListener.TestListen();
+Console.WriteLine("done");
+Console.ReadLine();
